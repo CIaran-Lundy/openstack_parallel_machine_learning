fecfbb7f83d4010e20bccd034cf78b0e701ce320
Ciaran-Lundy <ciaranlundy@gmail.com>
2021-01-08 19:17:17 +0000

    adding in delete host changes file after changes are reversed

--- terraform_scripts/automated_cleanup.sh
+++ terraform_scripts/automated_cleanup.sh
@@ -6,9 +6,9 @@ terraform destroy
 
 etc_hosts_changes_file=etc_hosts_changes_file.txt
 etc_hosts_file="/etc/hosts"
-#etc_hosts_file="test_out.txt"
 while read line 
 do
 	del="$line" awk -i inplace '($0"") != ENVIRON["del"]' "$etc_hosts_file"
 done < $etc_hosts_changes_file
 
+rm $etc_hosts_changes_file
